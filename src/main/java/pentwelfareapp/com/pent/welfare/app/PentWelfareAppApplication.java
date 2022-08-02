5e4cc495cbde6e3434f3661859b32583ba91c37b
= <vincent.tetteh@turntabl.io>
2022-08-02 20:38:56 +0000

    annotated webMmvcConfigurer as bean

--- src/main/java/pentwelfareapp/com/pent/welfare/app/PentWelfareAppApplication.java
+++ src/main/java/pentwelfareapp/com/pent/welfare/app/PentWelfareAppApplication.java
@@ -2,15 +2,19 @@ package pentwelfareapp.com.pent.welfare.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PentWelfareAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PentWelfareAppApplication.class, args);
 	}
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
